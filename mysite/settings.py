24c65c20242320d721da205a5e191aacd204fc07
Zac Baker <2024zbaker@tjhsst.edu>
2022-02-22 22:40:05 -0500

    New updates for Heroku deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '0x!b#(1*cd73w$&azzc6p+essg7v=g80ls#z&xcx*mpemx&@9$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
